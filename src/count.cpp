9fe14ab64a5b354328fafddf0635d2b44b7584b8
sankethchalla <sankethchalla@missionrnd.com>
2016-03-08 19:52:04 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -11,6 +11,31 @@ ERROR CASES: Return -1 in error cases.
 
 NOTES:
 */
+#include<stdio.h>
 int count(int num1, int num2) {
-	return 0;
+	int i,count = 0;
+	if (num1 == 0)
+	{
+		return -1;
+	}
+	if (num2 == 0)
+	{
+		return 0;
+	}
+	if (num1 == num2)
+	{
+		return count;
+	}
+	if (num1 < num2)
+	{
+		for (i = num1; i<=num2; i++)
+		{
+			if (i%num1 == 0)
+			{
+				count++;
+			}
+		}
+	}
+	
+	return count;
 }
